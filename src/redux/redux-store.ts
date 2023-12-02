193f1686be565c13f2854634b9e121cc94d69aca
SanuaKligan <SandorKligan125@gmail.com>
2023-12-02 21:54:28 +0300

    fix5

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,9 +1,4 @@
 import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
-// import appReducer from "./app-reducer";
-// import dialogsReducer from "./dialogs-reducer";
-// import sidebarReducer from "./sidebar-reducer";
-// import usersReducer from "./users-reducer";
-// import authReducer from "./auth-reducer";
 import catalogReducer from "./catalog-reducer";
 import thunkMiddleware from "redux-thunk";
 import {reducer as formReducer} from "redux-form";
@@ -13,25 +8,17 @@ import { useDispatch } from "react-redux";
 
 
 let rootReducer = combineReducers ({
-    // app: appReducer,
-    // dialogsPage: dialogsReducer,
-    // usersPage: usersReducer,
-    // sidebar: sidebarReducer,
-    // auth: authReducer,
     catalogPage: catalogReducer,
     form: formReducer
 });
 
-type RootReducerType = typeof rootReducer //(globalState: AppStateType) => AppStateType
+type RootReducerType = typeof rootReducer 
 export type AppStateType = ReturnType<RootReducerType>
 export type AppDispatch = typeof store.dispatch
 
 type PropertiesType<T> = T extends {[key: string]: infer U} ? U : never
 
 export type InferActionsTypes<T extends {[key: string]: (...args: any[]) => any}> = ReturnType<PropertiesType<T>>
-
-// let x: AppStateType
-//x.something
 
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
